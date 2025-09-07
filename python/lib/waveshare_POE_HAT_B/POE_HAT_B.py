dc0fbe64aad6e08353d41860ca232c572ccf1bcd
tedawf <hello@tedawf.com>
2025-09-07 22:10:38 +0800

    update poe hat to show pihole stats

--- python/lib/waveshare_POE_HAT_B/POE_HAT_B.py
+++ python/lib/waveshare_POE_HAT_B/POE_HAT_B.py
@@ -1,78 +1,112 @@
-import logging
-import sys
-import time
-import math
-import smbus2 as smbus
-import RPi.GPIO as GPIO
-
 import os
 import socket
-import fcntl
-import struct
-from PIL import Image,ImageDraw,ImageFont
+
+import psutil
+import requests
+import smbus2 as smbus
+from PIL import Image, ImageDraw, ImageFont
+
 from . import SSD1306
 
-
 show = SSD1306.SSD1306()
-show.Init();
+show.Init()
 dir_path = os.path.dirname(os.path.abspath(__file__))
 
 
-font = ImageFont.truetype(dir_path+'/Courier_New.ttf',13)
+font = ImageFont.truetype(dir_path + "/Courier_New.ttf", 13)
 
-image1 = Image.new('1', (show.width, show.height), "WHITE")
+image1 = Image.new("1", (show.width, show.height), "WHITE")
 draw = ImageDraw.Draw(image1)
+
+
 class POE_HAT_B:
-    def __init__(self,address = 0x20):
+    def __init__(self, address=0x20):
         self.i2c = smbus.SMBus(1)
-        self.address = address#0x20
+        self.address = address  # 0x20
         self.FAN_ON()
-        self.FAN_MODE = 0;
+        self.FAN_MODE = 0
+
     def FAN_ON(self):
         self.i2c.write_byte(self.address, 0xFE & self.i2c.read_byte(self.address))
-        
+
     def FAN_OFF(self):
         self.i2c.write_byte(self.address, 0x01 | self.i2c.read_byte(self.address))
-        
+
     def GET_IP(self):
-        s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-        s.connect(('8.8.8.8',80))
-        ip=s.getsockname()[0]
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
         s.close()
         return ip
-        
+
     def GET_Temp(self):
-        with open('/sys/class/thermal/thermal_zone0/temp', 'rt') as f:
-            temp = (int)(f.read() ) / 1000.0
+        with open("/sys/class/thermal/thermal_zone0/temp", "rt") as f:
+            temp = (int)(f.read()) / 1000.0
         return temp
-    
+
+    def GET_CPU_Usage(self):
+        return psutil.cpu_percent(interval=0.1)
+
+    def GET_Pihole_Stats(self):
+        try:
+            # Try Pi-hole API endpoint
+            response = requests.get("http://localhost/admin/api.php", timeout=2)
+            data = response.json()
+
+            queries_today = data.get("dns_queries_today", 0)
+            ads_blocked_today = data.get("ads_blocked_today", 0)
+            ads_percentage_today = data.get("ads_percentage_today", 0)
+
+            return queries_today, ads_blocked_today, ads_percentage_today
+        except:
+            # Fallback values if API fails
+            return 0, 0, 0
+
+    def format_number(self, num):
+        """Format numbers in K format for display"""
+        if num >= 1000000:
+            return f"{num/1000000:.1f}M"
+        elif num >= 1000:
+            return f"{num/1000:.1f}K"
+        else:
+            return str(num)
+
     def POE_HAT_Display(self, FAN_TEMP):
-        # show.Init()
-        # show.ClearBlack()
-        
-        image1 = Image.new('1', (show.width, show.height), "WHITE")
-        draw = ImageDraw.Draw(image1)  
-        ip = self.GET_IP()
+        image1 = Image.new("1", (show.width, show.height), "WHITE")
+        draw = ImageDraw.Draw(image1)
+
+        # Get system stats
         temp = self.GET_Temp()
-        draw.text((0,1), 'IP:'+str(ip), font = font, fill = 0)
-        draw.text((0,15), 'Temp:'+ str(((int)(temp*10))/10.0), font = font, fill = 0)
-        if(temp>=FAN_TEMP):
+        cpu_usage = self.GET_CPU_Usage()
+
+        # Get Pi-hole stats
+        total_queries, blocked_queries, block_percentage = self.GET_Pihole_Stats()
+
+        # Determine fan status first for display
+        if temp >= FAN_TEMP:
             self.FAN_MODE = 1
-            
-        elif(temp<FAN_TEMP-2):
+        elif temp < FAN_TEMP - 2:
             self.FAN_MODE = 0
-        
-        if(self.FAN_MODE == 1):
-            draw.text((77,16), 'FAN:ON', font = ImageFont.truetype(dir_path+'/Courier_New.ttf',12), fill = 0)
+
+        fan_status = "Y" if self.FAN_MODE == 1 else "N"
+
+        # Format the display strings
+        # Line 1: blocked/total percentage (format: 1.2K/5.6K 22%)
+        blocked_str = self.format_number(blocked_queries)
+        total_str = self.format_number(total_queries)
+        line1 = f"{blocked_str}/{total_str} {int(block_percentage)}%"
+
+        # Line 2: Temperature, CPU, and Fan (format: T:40.5 C:15% F:N)
+        line2 = f"T:{temp:.1f} C:{int(cpu_usage)}% F:{fan_status}"
+
+        # Draw the text
+        draw.text((0, 1), line1, font=font, fill=0)
+        draw.text((0, 15), line2, font=font, fill=0)
+
+        # Control the fan based on determined status
+        if self.FAN_MODE == 1:
             self.FAN_ON()
         else:
-            draw.text((77,16), 'FAN:OFF', font = ImageFont.truetype(dir_path+'/Courier_New.ttf',12), fill = 0)
             self.FAN_OFF()
+
         show.ShowImage(show.getbuffer(image1))
-
-
-
-
-
-
-
--- python/lib/waveshare_POE_HAT_B/SSD1306.py
+++ python/lib/waveshare_POE_HAT_B/SSD1306.py
@@ -6,7 +6,7 @@
 # *----------------
 # * | This version:   V1.0
 # * | Date        :   2019-11-14
-# * | Info        :   
+# * | Info        :
 # ******************************************************************************/
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documnetation files (the "Software"), to deal
@@ -28,22 +28,21 @@
 #
 
 from smbus2 import SMBus
-import time
-import numpy as np
+
 
 class SSD1306(object):
-    def __init__(self, width=128, height=32, addr=0x3c):
+    def __init__(self, width=128, height=32, addr=0x3C):
         self.width = width
         self.height = height
         self.Column = width
-        self.Page = int(height/8)
+        self.Page = int(height / 8)
         self.addr = addr
         self.bus = SMBus(1)
 
-    def SendCommand(self, cmd):# write command
+    def SendCommand(self, cmd):  # write command
         self.bus.write_byte_data(self.addr, 0x00, cmd)
 
-    def SendData(self, cmd):# write ram
+    def SendData(self, cmd):  # write ram
         self.bus.write_byte_data(self.addr, 0x40, cmd)
 
     def Closebus(self):
@@ -52,94 +51,86 @@ class SSD1306(object):
     def Init(self):
         self.SendCommand(0xAE)
 
-        self.SendCommand(0x40) # set low column address
-        self.SendCommand(0xB0) # set high column address
+        self.SendCommand(0x40)  # set low column address
+        self.SendCommand(0xB0)  # set high column address
 
-        self.SendCommand(0xC8) # not offset
+        self.SendCommand(0xC8)  # not offset
 
         self.SendCommand(0x81)
-        self.SendCommand(0xff)
+        self.SendCommand(0xFF)
 
-        self.SendCommand(0xa1)
+        self.SendCommand(0xA1)
 
-        self.SendCommand(0xa6)
+        self.SendCommand(0xA6)
 
-        self.SendCommand(0xa8)
-        self.SendCommand(0x1f)
+        self.SendCommand(0xA8)
+        self.SendCommand(0x1F)
 
-        self.SendCommand(0xd3)
+        self.SendCommand(0xD3)
         self.SendCommand(0x00)
 
-        self.SendCommand(0xd5)
-        self.SendCommand(0xf0)
+        self.SendCommand(0xD5)
+        self.SendCommand(0xF0)
 
-        self.SendCommand(0xd9)
+        self.SendCommand(0xD9)
         self.SendCommand(0x22)
 
-        self.SendCommand(0xda)
+        self.SendCommand(0xDA)
         self.SendCommand(0x02)
 
-        self.SendCommand(0xdb)
+        self.SendCommand(0xDB)
         self.SendCommand(0x49)
 
-        self.SendCommand(0x8d)
+        self.SendCommand(0x8D)
         self.SendCommand(0x14)
 
-        self.SendCommand(0xaf)
+        self.SendCommand(0xAF)
 
     def ClearBlack(self):
         for i in range(0, self.Page):
-            self.SendCommand(0xb0 + i)
+            self.SendCommand(0xB0 + i)
             self.SendCommand(0x00)
-            self.SendCommand(0x10) 
+            self.SendCommand(0x10)
             for j in range(0, self.Column):
                 self.SendData(0x00)
 
     def ClearWhite(self):
         for i in range(0, self.Page):
-            self.SendCommand(0xb0 + i)
+            self.SendCommand(0xB0 + i)
             self.SendCommand(0x00)
-            self.SendCommand(0x10) 
+            self.SendCommand(0x10)
             for j in range(0, self.Column):
-                self.SendData(0xff)
+                self.SendData(0xFF)
 
     def getbuffer(self, image):
-        buf = [0xff] * (self.Page * self.Column)
-        image_monocolor = image.convert('1')
+        buf = [0xFF] * (self.Page * self.Column)
+        image_monocolor = image.convert("1")
         imwidth, imheight = image_monocolor.size
         pixels = image_monocolor.load()
-        if(imwidth == self.width and imheight == self.height):
+        if imwidth == self.width and imheight == self.height:
             # print ("Horizontal screen")
             for y in range(imheight):
                 for x in range(imwidth):
                     # Set the bits for the column of pixels at the current position.
                     if pixels[x, y] == 0:
                         buf[x + int(y / 8) * self.width] &= ~(1 << (y % 8))
-        elif(imwidth == self.height and imheight == self.width):
+        elif imwidth == self.height and imheight == self.width:
             # print ("Vertical screen")
             for y in range(imheight):
                 for x in range(imwidth):
                     newx = y
                     newy = self.height - x - 1
                     if pixels[x, y] == 0:
-                        buf[(newx + int(newy / 8 )*self.width) ] &= ~(1 << (y % 8))
+                        buf[(newx + int(newy / 8) * self.width)] &= ~(1 << (y % 8))
         for x in range(self.Page * self.Column):
             buf[x] = ~buf[x]
         return buf
-            
+
     def ShowImage(self, pBuf):
-        for i in range(0, self.Page):            
-            self.SendCommand(0xB0 + i) # set page address
-            self.SendCommand(0x00) # set low column address
-            self.SendCommand(0x10) # set high column address
+        for i in range(0, self.Page):
+            self.SendCommand(0xB0 + i)  # set page address
+            self.SendCommand(0x00)  # set low column address
+            self.SendCommand(0x10)  # set high column address
             # write data #
             for j in range(0, self.Column):
-                self.SendData(pBuf[j+self.width*i])
-
-
-    
-    
-    
-    
-    
-    
+                self.SendData(pBuf[j + self.width * i])
